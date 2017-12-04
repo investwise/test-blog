127d3df4594356b4dc4bf8b744db04c786ddd92c
invetwise <hello@investwise.io>
2017-12-04 01:14:15 +0000

    commit after blog template

--- www/urls.py
+++ www/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('cms.urls')),
+    url(r'^', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
